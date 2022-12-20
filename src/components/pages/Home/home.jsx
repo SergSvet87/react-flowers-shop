492a8183e081c158b4b748c744db23e1c05df0e9
AnyaRatusnyak <anyaaboyko@gmail.com>
2022-12-21 00:58:36 +0200

    fixed the post

--- src/components/pages/Home/home.jsx
+++ src/components/pages/Home/home.jsx
@@ -1,38 +1,42 @@
-import React from 'react';
-import './home.scss';
-import {Post} from './Post/post';
-import {Footer} from '../../footer/footer';
-import {GoodsCard} from '../../ui/goods/goodsCard';
-import {MainTitle} from './MainTitle/MainTitle';
+import React from "react";
+import "./home.scss";
+import { Post } from "./Post/post";
+import { Footer } from "../../Footer/footer";
+import { GoodsCard } from "../../ui/goods/goodsCard";
+import { MainTitle } from "./MainTitle/MainTitle";
 
-let arr = Array(4).fill({name: 'Flower\'s Name', price: 0});
+let arr = Array(4).fill({ name: "Flower's Name", price: 0 });
 let bestSellers = arr.map((elem) => (
-    <div className="col-3">
-        <GoodsCard name={elem.name} price={elem.price}/>
-    </div>
+  <div className="col-3">
+    <GoodsCard name={elem.name} price={elem.price} />
+  </div>
 ));
 
 export const Home = () => {
-    return (
-        <div className="container">
-            <MainTitle />
-            <div>Best selers</div>
-            <div className="row">{bestSellers}</div>
-            <div>Latest posts</div>
-            <div className="row">
-                <div className="col-4">
-                    <Post/>
-                </div>
-                <div className="col-4">post</div>
-                <div className="col-4">post</div>
-            </div>
-            <div>Comments</div>
-            <div className="row">
-                <div className="col-4">Comment</div>
-                <div className="col-4">Comment</div>
-                <div className="col-4">Comment</div>
-            </div>
-            <Footer/>
+  return (
+    <div className="container">
+      <MainTitle />
+      <div>Best selers</div>
+      <div className="row">{bestSellers}</div>
+      <div>Latest posts</div>
+      <div className="row">
+        <div className="col-4">
+          <Post />
         </div>
-    );
+        <div className="col-4">
+          <Post />
+        </div>
+        <div className="col-4">
+          <Post />
+        </div>
+      </div>
+      <div>Comments</div>
+      <div className="row">
+        <div className="col-4">Comment</div>
+        <div className="col-4">Comment</div>
+        <div className="col-4">Comment</div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
