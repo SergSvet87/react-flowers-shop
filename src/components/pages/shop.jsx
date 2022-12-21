371d4cc71b711aaf5b347d9cddcb6de89930ccd7
Ten-Do <rydenko.urii@mail.ru>
2022-12-21 05:23:42 +0300

    добавил отступ сверху к футеру и убрал из shop лишнее

--- src/components/pages/shop.jsx
+++ src/components/pages/shop.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Catalog } from "./catalog";
-import { SelectedFlower } from "../selected-flower/selected-flower";
 
 export const Shop = () => {
   return (
     <section id="shop" className="shop"> 
       <Catalog />
-      <SelectedFlower />
     </section>
   );
 };
